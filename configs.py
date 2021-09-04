e076df59960ac7edb1743ba3d92e6dce4698634a
Guillaume Lemieux <guilemieux@gmail.com>
2021-09-03 21:29:50 -0400

    Create basic config

---
+++ configs.py
@@ -0,0 +1,19 @@
+import configparser
+import os
+
+ENV = os.environ.get('ENV', 'DEV')
+if ENV not in ('DEV', 'STAGING', 'PROD'):
+    raise RuntimeError(
+        f'Environment "{ENV}" is not recognized. Check your environment variable to make'
+        'sure that the environment variable ENV is set to one of "DEV", "STAGING" or "PROD".'
+    )
+
+config = configparser.ConfigParser()
+config.read(f"./configs/{ENV.lower()}.ini")
+
+db_config = config['Database']
+POSTGRES_USER = db_config.get('postgres_user')
+POSTGRES_PASSWORD = db_config.get('postgres_password')
+POSTGRES_HOST = db_config.get('postgres_host')
+POSTGRES_PORT = db_config.get('postgres_port', 5432)
+POSTGRES_URL = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}'
